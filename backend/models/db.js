55e69c465f72666c84e616440b94075b521f4eff
Evilpixi <tatosalvi@gmail.com>
2024-02-20 16:20:01 -0300

    created many to many

--- backend/models/db.js
+++ backend/models/db.js
@@ -1,28 +1,54 @@
 const db = {}
+const { Sequelize, DataTypes } = require('sequelize')
+const config = require("../../config")
+const sequelize = new Sequelize(config.database.connectionString)
+
 const Material = require("./material.model")
 const Publisher = require("./publisher.model")
 const Category = require("./category.model")
 const Author = require("./author.model")
 const Supplier = require("./supplier.model")
 
-db.sync = () => {
-  Material.sync({ force: true })
-  Publisher.sync({ force: true })
-  Category.sync({ force: true })
-  Author.sync({ force: true })
-  Supplier.sync({ force: true })
+db.sync = async () => {
+  await Material.sync({ force: true })
+  await Publisher.sync({ force: true })
+  await Category.sync({ force: true })
+  await Author.sync({ force: true })
+  await Supplier.sync({ force: true })
+
+
+
+
+  Publisher.hasMany(Material, { foreignKey: "publisher_id" })
+  Material.belongsTo(Publisher, { foreignKey: "publisher_id" })
+
+  Category.hasMany(Material, { foreignKey: "category_id" })
+  Material.belongsTo(Category, { foreignKey: "category_id" })
+
+  Author.hasMany(Material, { foreignKey: "author_id" })
+  Material.belongsTo(Author, { foreignKey: "author_id" })
+  await Material.sync({ force: true })
+
+
+
+  const MaterialSupplier = sequelize.define("MaterialSupplier", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+    }
+  })
+  await MaterialSupplier.sync({ force: true })
+
+  Material.belongsToMany(Supplier, { through: MaterialSupplier })
+  Supplier.belongsToMany(Material, { through: MaterialSupplier })
+  MaterialSupplier.belongsTo(Material, { foreignKey: "material_id" })
+  MaterialSupplier.belongsTo(Supplier, { foreignKey: "supplier_id" })
+  Material.hasMany(MaterialSupplier, { foreignKey: "material_id" })
+  Supplier.hasMany(MaterialSupplier, { foreignKey: "supplier_id" })
+  await MaterialSupplier.sync({ force: true })
 }
-
-Publisher.hasMany(Material, { foreignKey: "publisher_id" })
-Material.belongsTo(Publisher, { foreignKey: "publisher_id" })
-
-Category.hasMany(Material, { foreignKey: "category_id" })
-Material.belongsTo(Category, { foreignKey: "category_id" })
-
-Author.hasMany(Material, { foreignKey: "author_id" })
-Material.belongsTo(Author, { foreignKey: "author_id" })
-
-Material.belongsToMany(Supplier, { through: })
 
 
 module.exports = db
